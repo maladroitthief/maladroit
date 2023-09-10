b36f2a57c1fccc4c929cc132af846d1462fcd801
Ian Weller <maladroitthief@gmail.com>
2023-09-09 21:40:51 -0400

    feat: Initial web app
    - Adds basic go web server
    - Adds htmx
    - Adds tailwind

---
+++ cmd/maladroit/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(s.ListenAndServe())
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+  tmpl := template.Must(template.ParseFiles("web/app/index.html"))
+  tmpl.Execute(w, "")
+}
